109259afab1ef466701279500c7816f7f1c712a2
vagrant <vagrant@proyecto>
2018-05-23 02:38:53 +0000

    exceptions finales

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,9 +4,16 @@ namespace App\Exceptions;
 
 use Exception;
 use App\Traits\ApiResponser;
+use Illuminate\Database\QueryException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Foundation\Exceptions\unauthenticated;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -55,25 +62,76 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+
+                // verifica errores de laravel en formato Json
          if ($exception instanceof ValidationException) {
             return $this->convertValidationExceptionToResponse($exception, $request);
          }
 
+            //verifica si existe el dato en la base de datos envie error en Json
          if ($exception instanceof ModelNotFoundException) {
             $modelo = strtolower(class_basename($exception->getModel()));
              return $this->errorResponse("no existe ninguna instancia de {$modelo} con el id especificado",404);
          }
-         
-        return parent::render($request, $exception);
+
+         // Verifica si el usuario esta autenticado de lo contrario envía error en Json 
+         if ($exception instanceof AuthenticationException) {
+            return $this->unauthenticated($request, $$exception);
+        }
+
+            // el ususario no tiene autoización a esta página de lo contrario envía error en Json 
+        if ($exception instanceof AuthorizationException) {
+            return $this->errorResponse('No posee autorización',403);
+        }
+            // url no existe de lo contrario envía error en Json 
+        if ($exception instanceof NotFoundHttpException) {
+            return $this->errorResponse('No se encontro la Url especificada',404);
+        }
+
+           // Verifica el metodo (POST|GET|PUT) petición si es el correcto de lo contrario envía error en Json 
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return $this->errorResponse('El metodo especificado en la petición no es valido',405);
+        }
+
+          // envía cualquier otro error en Json 
+        if ($exception instanceof HttpException) {
+            return $this->errorResponse($exception->getMessage(), $exceptions->getStatusCode());
+        }
+
+
+
+          // error al eliminar un recurso que esta relacionado con otro
+        if ($exception instanceof QueryException) {
+            $codigo = $exception->errorInfo[1];
+
+            if($codigo == 1451){
+            return $this->errorResponse('No se puede eliminar de forma permantente este recurso  por que está relacionado con otro', 409);
+        }
+
+    }
+            //verifica si el archivo config/app -> 'debug' se encuentra en false
+        if (config('app.debug')){
+           return parent::render($request, $exception);
+                }
+
+                
+          // Verifica otro error no contemplado en la Api
+         return $this->errorResponse('Falla Inesperada. Intente luego', 500);
+        
     }
 
-    /**
-     * Create a response object from the given validation exception.
-     *
-     * @param  \Illuminate\Validation\ValidationException  $e
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
+
+
+            // --------------------  Funciones ------------------------   
+
+        // verifica si el usuario esta autenticado
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->errorResponse('No autenticado',201);
+    }
+
+   
+        // envia errores en formato Json
     protected function convertValidationExceptionToResponse(ValidationException $e, $request)
     {
         
